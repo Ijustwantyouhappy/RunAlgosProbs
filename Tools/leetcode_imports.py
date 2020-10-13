1612beebbfe6996f4892a65fa68b00ffed47d5c5
run[windows] <never_mind_sl@163.com>
2020-10-13 13:35:29 +0800

    add leetcode_imports.py in Tools

---
+++ Tools/leetcode_imports.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2020/10/12 23:08
+# @Author   : Run 
+# @File     : leetcode_imports.py
+# @Software : PyCharm
+
+"""
+导入LeetCode刷题常用的python包
+"""
+
+import re
+from collections import *
+from itertools import *
+from typing import List
+from bisect import *
+from string import ascii_lowercase
