801f879bb7b94bb815f72dcbf11e0991bc6d5f49
run[windows] <never_mind_sl@163.com>
2020-12-03 12:18:50 +0800

    prime numbers

--- Tools/leetcode_imports.py
+++ Tools/leetcode_imports.py
@@ -12,6 +12,7 @@ import re
 from collections import *
 from sortedcontainers import *
 from itertools import *
+from scipy.special import comb, perm  # 排列组合计数，结果为浮点型
 from typing import List
-from bisect import *
+from bisect import *  # 二分查找及插值
 from string import ascii_lowercase
